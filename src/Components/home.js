cafa58f6818c08505c6971a3b823b20bea0d56d6
anti-lex <a_valsamos@fanshaweonline.ca>
2023-02-10 19:09:58 -0500

    for some reason it did not commit properly

---
+++ src/Components/home.js
@@ -0,0 +1,12 @@
+import * as React from "react" 
+import { NavLink } from "react-router-dom"
+
+function Home() {
+    return (
+    <>
+    <h1>this is a homepage!</h1>
+    </>
+    )
+}
+
+export default Home; 
---
+++ src/Components/test.js
@@ -0,0 +1,12 @@
+import * as React from "react" 
+import { NavLink } from "react-router-dom"
+
+function Test() {
+    return (
+    <>
+    <h1>this is a test!</h1>
+    </>
+    )
+}
+
+export default Test; 
