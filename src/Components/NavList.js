2fe96ab1cd7793662c6950a2c13c7d27ab421143
anti-spades <a_valsamos@fanshaweonline.ca>
2023-02-12 13:58:26 -0500

    fixed nav!!

--- src/Components/NavList.js
+++ src/Components/NavList.js
@@ -1,5 +1,5 @@
 import React from "react" 
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom"
 import Home from "./home";
 import Test from "./test";
 function NavList() { 
@@ -7,11 +7,19 @@ function NavList() {
         <>
         <div className="navbar">
         <BrowserRouter>
+            <ul className="top-nav">
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/Test">Test</Link>
+                </li>
+            </ul>
+
             <Routes>
-                <Route path="/" element={<Home/>}>
-                    <Route index element={<Home/>} />
-                    <Route path ="Test" element={<Test />} />
-                </Route>
+                <Route path="/" element={<Home/>}/>
+                <Route index element={<Home/>} />
+                <Route path ="/Test" element={<Test />} />
             </Routes>
         </BrowserRouter>
         </div>
