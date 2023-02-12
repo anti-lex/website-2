0f6a57b80124f2c14660c06e3c224798c19eb780
anti-spades <a_valsamos@fanshaweonline.ca>
2023-02-12 15:24:49 -0500

    changes to styles and getting rid of warning

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import NavList from './Components/NavList';
 
--- src/Components/test.js
+++ src/Components/test.js
@@ -1,6 +1,4 @@
 import * as React from "react" 
-import { NavLink } from "react-router-dom"
-
 function Test() {
     return (
     <>
