37e9d4a06ea64550ce22645d79d26e49ec27d674
anti-spades <a_valsamos@fanshaweonline.ca>
2023-02-13 00:46:55 -0500

    workin with icons

--- src/Components/NavList.js
+++ src/Components/NavList.js
@@ -2,27 +2,29 @@ import React from "react"
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom"
 import Home from "./home";
 import Test from "./test";
+import { IoHome } from "react-icons/io5";
+import { IoCard } from "react-icons/io5";
 function NavList() { 
     return (
         <>
-        <div className="navbar">
         <BrowserRouter>
+        <div className="navbar">
             <ul className="top-nav">
                 <li>
-                    <Link to="/">Home</Link>
+                    <Link to="/"><IoHome/>Home</Link>
                 </li>
                 <li>
-                    <Link to="/Test">Test</Link>
+                    <Link to="/Test"><IoCard className="react-icons"/>Test</Link>
                 </li>
             </ul>
-
+        </div>
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route index element={<Home/>} />
                 <Route path ="/Test" element={<Test />} />
             </Routes>
         </BrowserRouter>
-        </div>
+
         </>
     );
 }
--- src/Components/home.js
+++ src/Components/home.js
@@ -1,8 +1,10 @@
 import * as React from "react" 
+
 function Home() {
     return (
     <>
     <h1>this is a homepage!</h1>
+    <p>This is a test</p>
     </>
     )
 }
